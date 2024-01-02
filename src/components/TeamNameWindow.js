2dc2fbd1fa2926a8b0a9cd086e06feae2eb6c731
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-01-02 01:17:23 -0600

    added link and styling

--- src/components/TeamNameWindow.js
+++ src/components/TeamNameWindow.js
@@ -1,6 +1,7 @@
 import {useGame} from "../context/GameContext";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import {Link} from "react-router-dom";
 
 function TeamNameWindow() {
     const {setTeam1, setTeam2, team1, team2, handleClose} = useGame();
@@ -11,14 +12,14 @@ function TeamNameWindow() {
                 <Modal.Body style={{backgroundColor: '#f3e640'}}>
                     <h3 className='text-center text-decoration-underline'>Enter Team Names</h3>
 
-                    <div className='d-flex flex-column my-2'>
+                    <div className='d-flex flex-column my-2 px-3'>
                         <label htmlFor="team1">Team 1:</label>
                         <input type="text"
                                value={team1}
                                onChange={(e) => setTeam1(e.target.value)}/>
                     </div>
 
-                    <div className='d-flex flex-column my-4'>
+                    <div className='d-flex flex-column my-4 px-3'>
                         <label htmlFor="team2">Team 2:</label>
                         <input type="text"
                                value={team2}
@@ -29,9 +30,11 @@ function TeamNameWindow() {
                         <Button variant="secondary" onClick={handleClose}>
                             Close
                         </Button>
-                        <Button variant="primary" onClick={handleClose}>
-                            Let's Play!
-                        </Button>
+                        <Link to={'/game'}>
+                            <Button variant="primary" onClick={handleClose}>
+                                Let's Play!
+                            </Button>
+                        </Link>
                     </div>
                 </Modal.Body>
             </Modal>
