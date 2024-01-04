e01e5d36f4b00e49e6b96659c2e1a3720e32c6ef
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-01-03 23:38:48 -0600

    added round 1 and 2 game data and state for scores

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -2,10 +2,95 @@ import {createContext, useContext, useState} from "react";
 
 const GameContext = createContext(undefined);
 
+const round1 = {
+    question: 'Name an occupation that begins with the letter "J."',
+    answers: [
+        {
+            answer: 'Janitor',
+            points: 62,
+            revealed: false
+        },
+        {
+            answer: 'Judge',
+            points: 19,
+            revealed: false
+        },
+        {
+            answer: 'Jeweler',
+            points: 5,
+            revealed: false
+        },
+        {
+            answer: 'Jockey',
+            points: 4,
+            revealed: false
+        },
+        {
+            answer: 'Journalist',
+            points: 4,
+            revealed: false
+        },
+        {
+            answer: 'Juggler',
+            points: 3,
+            revealed: false
+        },
+        {
+            answer: 'Jailer',
+            points: 2,
+            revealed: false
+        },
+        {
+            answer: 'Jeweler',
+            points: 1,
+            revealed: false
+        },
+    ]
+}
+const round2 = {
+    question: 'Name something with teeth',
+    answers: [
+        {
+            answer: 'Animals',
+            points: 66,
+            revealed: false
+        },
+        {
+            answer: 'People',
+            points: 19,
+            revealed: false
+        },
+        {
+            answer: 'A Comb',
+            points: 8,
+            revealed: false
+        },
+        {
+            answer: 'A Zipper',
+            points: 4,
+            revealed: false
+        },
+        {
+            answer: 'A Saw',
+            points: 2,
+            revealed: false
+        },
+    ]
+}
+
 function GameProvider({children}) {
-    const [team1, setTeam1] = useState("");
-    const [team2, setTeam2] = useState("");
+    const gameData = [round1, round2]
+    const [team1, setTeam1] = useState("Johnson");
+    const [team2, setTeam2] = useState("Martin");
     const [show, setShow] = useState(false);
+    const [t1Score, setT1Score] = useState(0);
+    const [t2Score, setT2Score] = useState(0);
+    const [roundScore, setRoundScore] = useState(0);
+    const [roundQuestionHidden, setRoundQuestionHidden] = useState(true);
+
+    const showQuestion = () => {
+        setRoundQuestionHidden(false);
+    }
 
     const handleClose = () => {
         setShow(false);
@@ -17,11 +102,21 @@ function GameProvider({children}) {
 
     return (
         <GameContext.Provider value={{
+            gameData,
             team1,
             setTeam1,
+            t1Score,
+            setT1Score,
+            t2Score,
+            setT2Score,
             team2,
             setTeam2,
             show,
+            roundScore,
+            setRoundScore,
+            roundQuestionHidden,
+            setRoundQuestionHidden,
+            showQuestion,
             handleShow,
             handleClose
         }}>
