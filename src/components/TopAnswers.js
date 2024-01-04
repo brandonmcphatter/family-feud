554e7e2ac0b432017ea90ed6eac7fa9680a75dd4
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-01-03 23:40:18 -0600

    formatted and functional with click to show answer

---
+++ src/components/TopAnswers.js
@@ -0,0 +1,35 @@
+import Answer from "./Answer";
+import {useGame} from "../context/GameContext";
+
+
+function TopAnswers() {
+    const {gameData} = useGame();
+
+    return (
+        <div className='d-grid w-75'>
+
+            <div className='row'>
+                <Answer answer={gameData[0].answers[0].answer} points={gameData[0].answers[0].points}>1</Answer>
+                <Answer answer={gameData[0].answers[4].answer} points={gameData[0].answers[4].points}>5</Answer>
+            </div>
+
+            <div className='row'>
+                <Answer answer={gameData[0].answers[1].answer} points={gameData[0].answers[1].points}>2</Answer>
+                <Answer answer={gameData[0].answers[5].answer} points={gameData[0].answers[5].points}>6</Answer>
+            </div>
+
+            <div className='row'>
+                <Answer answer={gameData[0].answers[2].answer} points={gameData[0].answers[2].points}>3</Answer>
+                <Answer answer={gameData[0].answers[6].answer || ''} points={gameData[0].answers[6].points || ''}>7</Answer>
+            </div>
+
+            <div className='row'>
+                <Answer answer={gameData[0].answers[3].answer} points={gameData[0].answers[3].points}>4</Answer>
+                <Answer answer={gameData[0].answers[7].answer || ''} points={gameData[0].answers[7].points || ''}>8</Answer>
+            </div>
+
+        </div>
+    )
+}
+
+export default TopAnswers;
