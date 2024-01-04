26ad8a4f80e5d5fa7b2a1ca937a22ba2fc08c1ec
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-01-03 23:38:22 -0600

    base for GameBoard.js

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,6 +1,15 @@
+import {useGame} from "../context/GameContext";
+import './GameBoard.css';
+
 function GameBoard() {
+    const {team1, team2} = useGame();
     return (
-        <div></div>
+        <div className='gameBoard '>
+            <div className='container w-50 d-flex justify-content-between'>
+            <h1 className='text-white'>{team1}</h1>
+            <h1 className='text-white'>{team2}</h1>
+            </div>
+        </div>
     );
 }
 
