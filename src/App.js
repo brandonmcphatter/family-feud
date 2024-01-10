ded05d9b427321ed3d375db2179808d143e13101
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-01-10 00:02:29 -0600

    changed routing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import HomeScreen from "./components/HomeScreen";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import SetupScreen from "./components/SetupScreen";
 import GameBoard from "./components/GameBoard";
-import AnswerBoard from "./components/AnswerBoard";
+import AnswerBoard from "./components/answerboard/AnswerBoard";
 
 export default function App() {
 
@@ -27,12 +27,15 @@ export default function App() {
                     </HomeScreen>
                 }/>
                 <Route path='setup' element={<SetupScreen/>}/>
-                <Route path='game' element={<GameBoard/>}/>
-                    <Route path='board' element={<AnswerBoard/>}/>
-                    <Route path='fast-money1' element={<h1>Fast Money</h1>}/>
-                    <Route path='fast-money2' element={<h1>Fast Money</h1>}/>
-                    <Route path='final' element={<h1>Final</h1>}/>
-
+                <Route path='game' element={<div>game/</div>}/>
+                <Route path='scoreboard' element={<h1>Scoreboard</h1>}/>
+                <Route path='round-1' element={<AnswerBoard rd={0}/>}/>
+                <Route path='round-2' element={<AnswerBoard rd={1}/>}/>
+                <Route path='round-3' element={<AnswerBoard rd={2}/>}/>
+                <Route path='sudden-death' element={<h1>Sudden Death</h1>}/>
+                <Route path='fast-money1' element={<h1>Fast Money</h1>}/>
+                <Route path='fast-money2' element={<h1>Fast Money</h1>}/>
+                <Route path='final' element={<h1>Final</h1>}/>
 
             </Routes>
 
