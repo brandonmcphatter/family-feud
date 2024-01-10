2f079006cc707c4aa89869f28f93a91dea6d40d6
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-01-10 00:02:48 -0600

    still working

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,14 +1,10 @@
-import {useGame} from "../context/GameContext";
 import './GameBoard.css';
+import AnswerBoard from "./answerboard/AnswerBoard";
 
-function GameBoard() {
-    const {team1, team2} = useGame();
+function GameBoard({children}) {
     return (
-        <div className='gameBoard '>
-            <div className='container w-50 d-flex justify-content-between'>
-            <h1 className='text-white'>{team1}</h1>
-            <h1 className='text-white'>{team2}</h1>
-            </div>
+        <div >
+            <AnswerBoard/>
         </div>
     );
 }
