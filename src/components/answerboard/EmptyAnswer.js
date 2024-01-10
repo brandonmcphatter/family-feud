80a9a202dd914f92af11928df7aa34c9dd1b6ba4
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-01-09 23:59:26 -0600

    new component

---
+++ src/components/answerboard/EmptyAnswer.js
@@ -0,0 +1,12 @@
+
+function EmptyAnswer() {
+    return (
+        <div className=' hidden-answer'>
+            <div className='inner-box d-flex justify-content-center align-items-center'>
+                <span className=' text-center display-2 fw-bolder'></span>
+            </div>
+        </div>
+    );
+}
+
+export default EmptyAnswer;
