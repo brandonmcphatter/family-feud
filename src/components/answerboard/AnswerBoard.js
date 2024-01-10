a316cdf1a8c80c6c11141a94888c490e43f6edea
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-01-10 00:02:20 -0600

    rd prop to pull correct answers

--- src/components/answerboard/AnswerBoard.js
+++ src/components/answerboard/AnswerBoard.js
@@ -2,10 +2,10 @@ import RoundScore from "./RoundScore";
 import TeamScore from "./TeamScore";
 import TopAnswers from "./TopAnswers";
 import RoundQuestion from "./RoundQuestion";
-import ActionBar from "./ActionBar";
-import {useGame} from "../context/GameContext";
+import ActionBar from "./../ActionBar";
+import {useGame} from "../../context/GameContext";
 
-function AnswerBoard() {
+function AnswerBoard({rd}) {
     const {t1Score, setT1Score, t2Score, setT2Score, roundScore} = useGame();
 
     return (
@@ -13,7 +13,7 @@ function AnswerBoard() {
             <RoundScore/>
             <div className='d-flex justify-content-evenly align-content-center'>
                 <TeamScore teamScore={t1Score} setScore={setT1Score} roundScore={roundScore}/>
-                <TopAnswers rd={0}/>
+                <TopAnswers rd={rd}/>
                 <TeamScore teamScore={t2Score} setScore={setT2Score} roundScore={roundScore}/>
             </div>
             <RoundQuestion/>
