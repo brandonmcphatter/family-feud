5c26f22d1560807f92b043a48394b50150d78b76
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-01-25 16:05:27 -0600

    mapping answer array to display fm answers

--- src/components/fastMoney/AnswerColumns.js
+++ src/components/fastMoney/AnswerColumns.js
@@ -1,25 +1,20 @@
 import FastMoneyAnswerBlock from "./FastMoneyAnswerBlock";
 import FastMoneyPoints from "./FastMoneyPoints";
+import {useGame} from "../../context/GameContext";
 
 export default function AnswerColumns() {
+    const {player1Answers, player2Answers, player1Points, player2Points} = useGame();
+
     return (
         <div className={'answer-columns d-flex gap-3'}>
             <div className={'col-6 d-flex flex-column gap-3'}>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
+                {player1Answers.map((answer, i,) => <FastMoneyAnswerBlock answer={player1Answers[i]} points={player1Points[i]} key={i}/>)}
                 <div className={'my-5 d-flex justify-content-end'}>
                     <FastMoneyPoints/>
                 </div>
             </div>
             <div className={'col-6 d-flex flex-column gap-3'}>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
+                {player2Answers.map((answer, i) => <FastMoneyAnswerBlock answer={player2Answers[i]} points={player2Points[i]} key={i}/>)}
             </div>
         </div>
     )
