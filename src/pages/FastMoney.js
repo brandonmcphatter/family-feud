241201bd2b16c61ba5d9842b27cf607c3b2a89df
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-01-24 20:19:40 -0600

    separated components

--- src/pages/FastMoney.js
+++ src/pages/FastMoney.js
@@ -1,4 +1,7 @@
 import '.././components/fastMoney/FastMoney.css'
+import AnswerArea from "../components/fastMoney/AnswerArea";
+import AnswerColumns from "../components/fastMoney/AnswerColumns";
+import FastMoneyToolbar from "../components/fastMoney/FastMoneyToolbar";
 
 export default function FastMoney() {
     return (
@@ -10,71 +13,9 @@ export default function FastMoney() {
                     </div>
                 </AnswerArea>
             </div>
-        </div>
-    )
-        ;
-}
-
-function AnswerArea({children}) {
-    return (
-        <div className='fast-money'>
-            {children}
-        </div>
-    )
-}
-
-function AnswerColumns() {
-    return (
-        <div className={'answer-columns d-flex gap-3'}>
-            <div className={'col-6 d-flex flex-column gap-3'}>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
-                <div className={'my-5 d-flex justify-content-end'}>
-                    <FastMoneyPoints/>
-                </div>
-            </div>
-            <div className={'col-6 d-flex flex-column gap-3'}>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
-                <FastMoneyAnswerBlock/>
-            </div>
-        </div>
-    )
-}
-
-function FastMoneyAnswerBlock() {
-    return (
-        <div className={'fast-money-answer d-flex gap-1 row'}>
-            <div className={'answer-block col-10 d-flex justify-content-start align-items-center'}>
-                <span className={'fw-bolder display-6'}></span>
-            </div>
-            <div className={'points-block col-2 d-flex justify-content-center align-items-center'}>
-                <span className={'fw-bolder display-5'}></span>
-            </div>
+            <FastMoneyToolbar/>
         </div>
     );
-}
-
-function FastMoneyPoints() {
-    return (
-        <div className={'d-flex justify-content-end total-points-column px-0'}>
-
-            <div className={'total-points-block d-flex  align-items-center gap-3'}>
-                <div className='total-text'>
-                    <span className={'fw-bolder display-4 ps-3'}>TOTAL</span>
-                </div>
-                <div className={'total-points d-flex justify-content-end'}>
-                    <span className={'fw-bolder display-3 pe-2'}></span>
-                </div>
-
-            </div>
-        </div>
-    )
 }
 
 
