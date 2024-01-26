c016a8e98816d6f514569ed434c03c06ee2864f5
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-01-26 12:32:32 -0600

    changed styling / used navigate instead of link

--- src/components/TeamNameWindow.js
+++ src/components/TeamNameWindow.js
@@ -1,40 +1,51 @@
 import {useGame} from "../context/GameContext";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
-import {Link} from "react-router-dom";
+
+import {useNavigate} from "react-router";
 
 function TeamNameWindow() {
     const {setTeam1, setTeam2, team1, team2, handleClose} = useGame();
-
+    const notReady = team1 === '' || team2 === '';
+    const navigate = useNavigate();
+    function moveToGame() {
+        navigate('/game')
+    }
     return (
-        <div>
-            <Modal  show={true} onHide={handleClose} >
-                <Modal.Body style={{backgroundColor: '#f3e640' , borderRadius: '15px'}}>
-                    <h3 className='text-center text-decoration-underline'>Enter Team Names</h3>
+        <div >
+            <Modal  show={true} onHide={handleClose}
+                    size="med"
+                    centered >
+                <Modal.Body style={{backgroundColor: 'darkgoldenrod', border: '5px solid white'}}>
+                    <div style={{height: '30vh'}}>
 
-                    <div className='d-flex flex-column my-2 px-3'>
-                        <label htmlFor="team1">Team 1:</label>
-                        <input type="text"
-                               value={team1}
-                               onChange={(e) => setTeam1(e.target.value)}/>
-                    </div>
 
-                    <div className='d-flex flex-column my-4 px-3'>
-                        <label htmlFor="team2">Team 2:</label>
-                        <input type="text"
-                               value={team2}
-                               onChange={(e) => setTeam2(e.target.value)}/>
-                    </div>
+                        <div className='d-flex flex-column my-2 px-3'>
+                            <label className={'fw-bold fst-italic h4 text-white'} htmlFor="team1">Team 1:</label>
+                            <input className={'text-center text-uppercase'}
+                                   type="text"
+                                   value={team1}
+                                   onChange={(e) => setTeam1(e.target.value)}/>
+                        </div>
 
-                    <div className='d-flex justify-content-evenly mt-4'>
-                        <Button variant="secondary" onClick={handleClose}>
-                            Cancel
-                        </Button>
-                        <Link to={'/game'}>
-                            <Button variant="primary" onClick={handleClose}>
-                                Let's Play!
+                        <div className='d-flex flex-column my-4 px-3'>
+                            <label className={'fw-bold fst-italic h4 text-white'} htmlFor="team2">Team 2:</label>
+                            <input className={'text-center text-uppercase'}
+                                   type="text"
+                                   value={team2}
+                                   onChange={(e) => setTeam2(e.target.value)}/>
+                        </div>
+
+                        <div className='d-flex justify-content-evenly mt-5'>
+                            <Button variant="danger" onClick={handleClose}>
+                                Cancel
                             </Button>
-                        </Link>
+
+                                <Button variant="success" onClick={moveToGame} disabled={notReady}>
+                                    Let's Play!
+                                </Button>
+
+                        </div>
                     </div>
                 </Modal.Body>
             </Modal>
