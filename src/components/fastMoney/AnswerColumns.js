0a334ef6addc4ccd24b7a9489105ed9282aef9cf
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-06 15:01:46 -0600

    styles / context added

--- src/components/fastMoney/AnswerColumns.js
+++ src/components/fastMoney/AnswerColumns.js
@@ -1,12 +1,13 @@
 import FastMoneyAnswerBlock from "./FastMoneyAnswerBlock";
 import FastMoneyPoints from "./FastMoneyPoints";
-import {useGame} from "../../context/GameContext";
+import {useFastMoney} from "../../context/FastMoneyContext";
+import styles from './AnswerColumns.module.css'
 
 export default function AnswerColumns() {
-    const {player1Answers, player2Answers, player1Points, player2Points} = useGame();
+    const {player1Answers, player2Answers, player1Points, player2Points} = useFastMoney();
 
     return (
-        <div className={'answer-columns d-flex gap-3'}>
+        <div className={`${styles.answerColumns} gap-3`}>
             <div className={'col-6 d-flex flex-column gap-3'}>
                 {player1Answers.map((answer, i,) => <FastMoneyAnswerBlock answer={player1Answers[i]} points={player1Points[i]} key={i}/>)}
                 <div className={'my-5 d-flex justify-content-end'}>
