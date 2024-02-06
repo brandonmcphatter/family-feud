d4a465d7d912ded66ae06491d6b6c55f7dea8285
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-06 14:57:25 -0600

    styles and functions added

--- src/pages/WinScreen.js
+++ src/pages/WinScreen.js
@@ -1,12 +1,37 @@
-import {Link} from "react-router-dom";
+import styles from './WinScreen.module.css'
+import {useGame} from "../context/GameContext";
+import {useNavigate} from "react-router";
+import {useFastMoney} from "../context/FastMoneyContext";
+import useSound from "use-sound";
+import theme from '../audio/FF-theme.mp3'
 
 export default function WinScreen() {
+    const {team1} = useGame();
+    const {resetFastMoney} = useFastMoney();
+    const navigate = useNavigate();
+    const [play] = useSound(theme, {volume: 1}, {interrupt: false});
+    function exitAndReset() {
+        resetFastMoney();
+        navigate('/')
+    }
+
     return (
-        <div>
-            <div>Winner</div>
-            <Link to={'/'}>
-                <button>End Game</button>
-            </Link>
+        <div className={`${styles.winScreen} animate__animated animate__slideInUp`} onClick={()=> play()}>
+            <div className={'d-flex flex-column justify-content-center align-items-center gap-3'}>
+                <h1>Team {team1}, congratulations!</h1>
+                <div className={`${styles.bigMoney} animate__animated animate__pulse animate__infinite`}>
+                    <span className={' '}>$20,000</span>
+                </div>
+                <div className={styles.moneyBag}>
+                    💰💰💰💰💰💰💰
+                </div>
+
+                <h4 className={'text-white'}>(not really)</h4>
+
+                <div>
+                    <button onClick={exitAndReset}>EXIT</button>
+                </div>
+            </div>
         </div>
     )
 }
