d9898c532eb73a07cf60c94be0d5440ec3f12ad2
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-06 14:48:57 -0600

    model for custom game data

---
+++ src/context/CustomGameData.js
@@ -0,0 +1,217 @@
+
+
+const roundOne = {
+    question: 'this is the test question',
+    answers: [
+        {
+            answer: 'helloooo',
+            points: 10,
+            revealed: false
+        },
+        {
+            answer: 'jump',
+            points: 20,
+            revealed: false
+        },
+        {
+            answer: 'fish',
+            points: 15,
+            revealed: false
+        },
+        {
+            answer: 'time',
+            points: 4,
+            revealed: false
+        },
+        {
+            answer: 'five',
+            points: 5,
+            revealed: false
+        },
+        {
+            answer: 'six',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: 'sev',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: 'eght',
+            points: 0,
+            revealed: false
+        },
+    ]
+};
+
+const roundTwo = {
+    question: 'for round 2',
+    answers: [
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+    ]
+};
+const roundThree = {
+    question: '',
+    answers: [
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+    ]
+};
+const roundFour = {
+    question: '',
+    answers: [
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+        {
+            answer: '',
+            points: 0,
+            revealed: false
+        },{
+            answer: '',
+            points: 0,
+            revealed: false
+        },{
+            answer: '',
+            points: 0,
+            revealed: false
+        },{
+            answer: '',
+            points: 0,
+            revealed: false
+        },{
+            answer: '',
+            points: 0,
+            revealed: false
+        },{
+            answer: '',
+            points: 0,
+            revealed: false
+        },{
+            answer: '',
+            points: 0,
+            revealed: false
+        },
+    ]
+};
+
+const suddenDeath = {
+    question: '',
+    answer: '',
+    points: 300
+};
+const fastMoneyRound = [
+    {
+        question: '',
+        answers: ['', '', '', '', ''],
+        points: [0, 0, 0, 0, 0, ]
+    },
+    {
+        question: '',
+        answers: ['', '', '', '', ''],
+        points: [0, 0, 0, 0, 0, ]
+    },
+    {
+        question: '',
+        answers: ['', '', '', '', ''],
+        points: [0, 0, 0, 0, 0, ]
+    },
+    {
+        question: '',
+        answers: ['', '', '', '', ''],
+        points: [0, 0, 0, 0, 0, ]
+    },
+    {
+        question: '',
+        answers: ['', '', '', '', ''],
+        points: [0, 0, 0, 0, 0, ]
+    },
+
+];
+
+const customGameData = [roundOne, roundTwo, roundThree, roundFour, suddenDeath, fastMoneyRound];
+
+
+
+export {customGameData}
