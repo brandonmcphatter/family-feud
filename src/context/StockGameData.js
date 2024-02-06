60dc5b318a5d20a8d8a01cef985b2bf4b6d219b8
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-06 14:51:27 -0600

    stock game data file

---
+++ src/context/StockGameData.js
@@ -0,0 +1,210 @@
+const round1 = {
+    question: 'Name an occupation that begins with the letter "J."',
+    answers: [
+        {
+            answer: 'Janitor',
+            points: 62,
+        },
+        {
+            answer: 'Judge',
+            points: 19,
+        },
+        {
+            answer: 'Jeweler',
+            points: 5,
+        },
+        {
+            answer: 'Jockey',
+            points: 4,
+        },
+        {
+            answer: 'Journalist',
+            points: 4,
+        },
+        {
+            answer: 'Juggler',
+            points: 3,
+        },
+        {
+            answer: 'Jailer',
+            points: 2,
+        },
+        {
+            answer: 'Jeweler',
+            points: 1,
+        },
+    ]
+};
+const round2 = {
+    question: 'Name something with teeth',
+    answers: [
+        {
+            answer: 'Animals',
+            points: 66,
+        },
+        {
+            answer: 'People',
+            points: 19,
+        },
+        {
+            answer: 'A Comb',
+            points: 8,
+        },
+        {
+            answer: 'A Zipper',
+            points: 4,
+        },
+        {
+            answer: 'A Saw',
+            points: 2,
+        },
+        {
+            answer: '',
+            points: 0,
+        },
+        {
+            answer: '',
+            points: 0,
+        },
+        {
+            answer: '',
+            points: 0,
+        },
+    ]
+};
+const round3 = {
+    question: 'Tell Me A Word That Rhymes with “Election” ',
+    answers: [
+        {
+            answer: 'Selection',
+            points: 32,
+        },
+        {
+            answer: 'Erection',
+            points: 23,
+        },
+        {
+            answer: 'Perfection',
+            points: 6,
+        },
+        {
+            answer: 'Protection',
+            points: 6,
+        },
+        {
+            answer: 'Detection',
+            points: 5,
+        },
+        {
+            answer: 'Rejection',
+            points: 4,
+        },
+        {
+            answer: 'Collection',
+            points: 4,
+        },
+        {
+            answer: '',
+            points: 0,
+        },
+    ],
+};
+const round4 = {
+    question: 'Tell Me A Word That Rhymes with “Election” ',
+    answers: [
+        {
+            answer: 'Selection',
+            points: 32,
+        },
+        {
+            answer: 'Erection',
+            points: 23,
+        },
+        {
+            answer: 'Perfection',
+            points: 6,
+        },
+        {
+            answer: 'Protection',
+            points: 6,
+        },
+        {
+            answer: '',
+            points: 0,
+        },
+        {
+            answer: '',
+            points: 0,
+        },
+        {
+            answer: '',
+            points: 0,
+        },
+        {
+            answer: '',
+            points: 0,
+        },
+    ],
+};
+const suddenDeath = {
+    question: 'Name something you wind up.',
+    answers: [
+        {
+            answer: 'Clock',
+            points: 300,
+        },
+        {
+            answer: '',
+            points: 0,
+        }, {
+            answer: '',
+            points: 0,
+        }, {
+            answer: '',
+            points: 0,
+        }, {
+            answer: '',
+            points: 0,
+        }, {
+            answer: '',
+            points: 0,
+        }, {
+            answer: '',
+            points: 0,
+        }, {
+            answer: '',
+            points: 0,
+        },
+    ]
+};
+const fastMoneyRound = [
+    {
+        question: 'Name something in a person’s closet that only comes out on special occasions.',
+        answers: ['Suit/Tux', 'Dress', 'Jewelry', 'Shoes', 'Shotgun'],
+        points: [35, 26, 10, 6, 4]
+    },
+    {
+        question: 'Name a popular Christmas song.',
+        answers: ['Jingle Bells', 'Silent Night', 'All I Want For Christmas', 'White Christmas', 'Deck The Halls'],
+        points: [40, 25, 15, 10, 10]
+    },
+    {
+        question: 'Name a fruit you might find in a smoothie.',
+        answers: ['Strawberry', 'Banana', 'Blueberry', 'Mango', 'Pineapple'],
+        points: [35, 30, 15, 10, 10]
+    },
+    {
+        question: 'Name a popular summer activity.',
+        answers: ['Swimming', 'Hiking', 'The Beach', 'Biking', 'Barbecue'],
+        points: [35, 30, 20, 10, 5]
+    },
+    {
+        question: 'Name an animal you might see at the zoo.',
+        answers: ['Elephant', 'Lion', 'Monkey', 'Giraffe', 'Zebra'],
+        points: [30, 25, 20, 15, 10]
+    }
+];
+
+const stockGameData = [round1, round2, round3, round4, suddenDeath, fastMoneyRound];
+
+export {stockGameData}
