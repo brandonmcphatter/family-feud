eb695de221ce94e3926092b9a9a031c2ced1e1c8
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-06 14:51:00 -0600

    context and styles imported sound fx added

--- src/components/fastMoney/FastMoneyAnswerBlock.js
+++ src/components/fastMoney/FastMoneyAnswerBlock.js
@@ -1,29 +1,37 @@
-import {useGame} from "../../context/GameContext";
 import {useEffect, useState} from "react";
+import {useFastMoney} from "../../context/FastMoneyContext";
+import styles from './FastMoneyAnswerBlock.module.css'
+import useSound from "use-sound";
+import showAnswer from '../../audio/fast-money-answer-reveal-101soundboards.mp3'
+import showPoints from '../../audio/family-feud-good-answer.mp3'
 
 export default function FastMoneyAnswerBlock({answer, points}) {
-    const {fastMoneyScore, setFastMoneyScore, showAnswers, setShowAnswers} = useGame();
+    const {fastMoneyScore, setFastMoneyScore, showAnswers, setShowAnswers} = useFastMoney();
     const [answerHidden, setAnswerHidden] = useState(true);
     const [pointHidden, setPointHidden] = useState(true);
+    const [playAnswer] = useSound(showAnswer, {volume: 1});
+    const [playPoints] = useSound(showPoints, {volume: 1});
 
     function showPlayerAnswer() {
+        playAnswer();
         setAnswerHidden(false);
     }
 
     function showPointsForAnswer() {
+        playPoints();
         setPointHidden(false);
         setFastMoneyScore(fastMoneyScore + points);
     }
 
     return (
-        <div className={'fast-money-answer d-flex gap-1 row'}>
+        <div className={`${styles.fastMoneyAnswer} gap-1 row`}>
             <div onClick={showPlayerAnswer}
-                 className={'answer-block col-10 d-flex justify-content-start align-items-center'}>
+                 className={`${styles.answerBlock} col-10 `}>
                 <span className={'fw-bolder display-6 text-uppercase'}>{showAnswers || answerHidden ? '' :
                     <Typewriter text={answer} delay={25}/>}</span>
             </div>
             <div onClick={showPointsForAnswer}
-                 className={'points-block col-2 d-flex justify-content-center align-items-center'}>
+                 className={`${styles.pointsBlock} col-2`}>
                 <span className={'fw-bolder display-5'}>{showAnswers || pointHidden ? '' : points}</span>
             </div>
         </div>
