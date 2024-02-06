facec579ef31fcc38b3f0b299e31dd3c283cde4e
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-06 14:56:04 -0600

    import styles

--- src/components/answerBoard/TeamScore.js
+++ src/components/answerBoard/TeamScore.js
@@ -1,4 +1,4 @@
-import './TeamScore.css';
+import styles from './TeamScore.module.css';
 
 function TeamScore({teamScore, setScore, roundScore}) {
 
@@ -7,9 +7,8 @@ function TeamScore({teamScore, setScore, roundScore}) {
     }
 
     return (
-        <div className='animate__animated animate__slideInLeft teamScore  d-flex justify-content-center align-items-center  '>
-            <div className='score fw-bolder display-1' onClick={addRoundToTeamScore}>{teamScore}</div>
-
+        <div className={`animate__animated animate__slideInLeft ${styles.teamScore}`}>
+            <div className={`${styles.score} display-1`} onClick={addRoundToTeamScore}>{teamScore}</div>
         </div>
     );
 }
