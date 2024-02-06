1119ca7d455305d68909b99241348f2706f4ba11
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-06 14:58:22 -0600

    logic refactored to map hidden and empty answers correctly

--- src/components/answerBoard/TopAnswers.js
+++ src/components/answerBoard/TopAnswers.js
@@ -1,4 +1,4 @@
-import './TopAnswers.css';
+import styles from './TopAnswers.module.css';
 import HiddenAnswer from "./HiddenAnswer";
 import {useGame} from "../../context/GameContext";
 import EmptyAnswer from "./EmptyAnswer";
@@ -8,39 +8,31 @@ function TopAnswers({rd}) {
     const {gameData} = useGame();
     const answers1 = gameData[rd].answers.slice(0, 4);
     const answers2 = gameData[rd].answers.slice(4, 8);
-    const first = answers1.length < 4;
-    const second = answers1.length < 3;
-    const third = answers1.length < 2;
-    const fourth = answers1.length === 0;
-    const oneBlank = answers2.length < 4;
-    const twoBlanks = answers2.length < 3;
-    const threeBlanks = answers2.length < 2;
-    const fourBlanks = answers2.length === 0;
+
     return (
-        <div className='top-answers d-grid'>
-            <div className='answer-area row row-cols-2  '>
-                <div className='first-col d-flex flex-column  justify-content-between'>
-                    {answers1.map((answer, index) =>
-                        (<HiddenAnswer answer={answer.answer} points={answer.points} key={index}>
-                            {index + 1}
-                        </HiddenAnswer>))}
-                    {first && <EmptyAnswer/>}
-                    {second && <EmptyAnswer/>}
-                    {third && <EmptyAnswer/>}
-                    {fourth && <EmptyAnswer/>}
+        <div className={styles.topAnswers}>
+            <div className='d-flex justify-content-center gap-3'>
+                <div className={styles.firstCol}>
+                    {answers1.map((answer, index) => {
+                        if (answer.answer === '') {
+                            return <EmptyAnswer/>
+                        } else return <HiddenAnswer answer={answer.answer}
+                                                    points={answer.points}
+                                                    key={index}>{index + 1}
+                        </HiddenAnswer>
+                    })}
                 </div>
 
 
-                <div className='second-col d-flex flex-column justify-content-between'>
-                    {answers2.map((answer, index) =>
-                        (<HiddenAnswer answer={answer.answer} points={answer.points} key={index}>
-                            {index + 5}
-                        </HiddenAnswer>))}
-
-                    {oneBlank && <EmptyAnswer/>}
-                    {twoBlanks && <EmptyAnswer/>}
-                    {threeBlanks && <EmptyAnswer/>}
-                    {fourBlanks && <EmptyAnswer/>}
+                <div className={styles.secondCol}>
+                    {answers2.map((answer, index) => {
+                        if (answer.answer === '') {
+                            return <EmptyAnswer/>
+                        } else return <HiddenAnswer answer={answer.answer}
+                                                    points={answer.points}
+                                                    key={index}>{index + 5}
+                        </HiddenAnswer>
+                    })}
                 </div>
             </div>
         </div>
