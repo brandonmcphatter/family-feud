5172c59afc33776ae2dc0180a48a999036f26851
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-06 14:52:10 -0600

    custom provider added

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {GameProvider} from "./context/GameContext";
+import {FastMoneyProvider} from "./context/FastMoneyContext";
+import {CustomGameProvider} from "./context/CustomGameContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <GameProvider>
-            <App/>
-        </GameProvider>
+        <CustomGameProvider>
+            <GameProvider>
+                <FastMoneyProvider>
+                    <App/>
+                </FastMoneyProvider>
+            </GameProvider>
+        </CustomGameProvider>
     </React.StrictMode>
 );
 
