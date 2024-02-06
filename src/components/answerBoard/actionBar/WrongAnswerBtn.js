97da0280635b63eb25ec14cdef87415ebe89602f
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-06 14:57:42 -0600

    sound added

--- src/components/answerBoard/actionBar/WrongAnswerBtn.js
+++ src/components/answerBoard/actionBar/WrongAnswerBtn.js
@@ -1,11 +1,18 @@
 import styles from './WrongAnswerBtn.module.css'
 import {useGame} from "../../../context/GameContext";
-
+import React from 'react';
+import useSound from "use-sound";
+import wrong from '../../../audio/buzzer.mp3'
 export default function WrongAnswerBtn({num}) {
     const {strikes, setStrikes, addStrike} = useGame();
-
+    const [play] = useSound(wrong, {volume: 1});
     const removeStrike = () => {
         setStrikes(strikes - 1);
+    }
+
+    const wrongAnswer = () => {
+        addStrike();
+        play();
     }
 
     return (
@@ -17,7 +24,7 @@ export default function WrongAnswerBtn({num}) {
             }
 
             {(strikes ===0 || strikes < num )&&
-            <button onClick={addStrike}
+            <button onClick={wrongAnswer}
                     className={`${styles.wrongAnswer} ${styles.off}  mx-2 text-center`}>
                 <span>&#10005;</span></button>
             }
