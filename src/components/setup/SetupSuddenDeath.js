842815f30253ec14af67c6e716f1e1faeb296dd0
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-09 15:28:57 -0600

    not using currently - may refactor

--- src/components/setup/SetupSuddenDeath.js
+++ src/components/setup/SetupSuddenDeath.js
@@ -1,10 +1,10 @@
 import SetupQuestion from "./SetupQuestion";
 import SetupAnswer from "./SetupAnswer";
 
-export default function SetupSuddenDeath() {
+export default function SetupSuddenDeath({question, setQuestions, answers, points, setPoints, setAnswers}) {
     return (
         <>
-            <SetupQuestion roundNum={5}/>
+            <SetupQuestion roundNum={4}/>
             <div className={'align-self-start'}>
                 <SetupAnswer answerNum={1}/>
             </div>
