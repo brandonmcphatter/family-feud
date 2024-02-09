5b0f2bc45d232f63bdfd3f9d3d1c18e17d6e1aa9
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-09 15:38:46 -0600

    refactored props and mapping

--- src/components/setup/SetupAnswerArea.js
+++ src/components/setup/SetupAnswerArea.js
@@ -1,26 +1,24 @@
 import SetupAnswer from "./SetupAnswer";
-import {useCustomGame} from "../../context/CustomGameContext";
 
 export default function SetupAnswerArea({rdNum, answers, points, setAnswers, setPoints}) {
-const firstFour = answers.slice(0,4);
-const lastFour = answers.slice(4,8);
-
-
-
-
-
+    const firstFour = answers.slice(0, 4);
+    const lastFour = answers.slice(4, 8);
 
     return (
         <div className={'d-flex gap-5'}>
             <div className={'d-flex flex-column gap-5'}>
                 {firstFour.map((a, i) => {
-                    return <SetupAnswer key={i} allAnswers={answers} allPoints={points} answer={a} setAnswers={setAnswers} points={points[i]} setPoints={setPoints} answerNum={i+1} rdNum={rdNum} />
+                    return <SetupAnswer key={i} allAnswers={answers} allPoints={points} answer={a}
+                                        setAnswers={setAnswers} points={points[i]} setPoints={setPoints}
+                                        answerNum={i + 1} rdNum={rdNum}/>
                 })}
             </div>
 
             <div className={'d-flex flex-column gap-5'}>
                 {lastFour.map((a, i) => {
-                    return <SetupAnswer key={i+5} allAnswers={answers} allPoints={points} answer={a} setAnswers={setAnswers} points={points[i+4]} setPoints={setPoints} answerNum={i+5} rdNum={rdNum} />
+                    return <SetupAnswer key={i + 5} allAnswers={answers} allPoints={points} answer={a}
+                                        setAnswers={setAnswers} points={points[i + 4]} setPoints={setPoints}
+                                        answerNum={i + 5} rdNum={rdNum}/>
                 })}
             </div>
 
