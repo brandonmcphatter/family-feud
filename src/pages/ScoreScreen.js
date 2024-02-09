ee22171780b252133e74a64203c985616ec1a94f
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-08 23:19:43 -0600

    using navigate instead of links

--- src/pages/ScoreScreen.js
+++ src/pages/ScoreScreen.js
@@ -1,28 +1,34 @@
-import {Link} from "react-router-dom";
-
+import {useNavigate} from "react-router";
+import familyFeudLogo from '../images/big-ff-logo.png'
 export default function ScoreScreen() {
+
+    const navigate = useNavigate();
+
     return (
-        <div className='d-flex justify-content-evenly'>
-            <Link to={'/game/round-1'}>
-                <button>Round 1</button>
-            </Link>
+        <div className='d-flex flex-column  gap-3'>
 
-            <Link to={'/game/round-2'}>
-                <button>Round 2</button>
-            </Link>
+                <img src={familyFeudLogo} alt={'Family Feud Logo'} width={'70%'}/>
 
-            <Link to={'/game/round-3'}>
-                <button>Round 3</button>
-            </Link>
-            <Link to={'/game/sudden-death'}>
-                <button>Sudden Death</button>
-            </Link>
-            <Link to={'/game/fast-money'}>
-                <button>Fast Money</button>
-            </Link>
-            <Link to={'/'}>
-                <button>Home</button>
-            </Link>
+            <div>
+            </div>
+
+            <div className={'d-flex flex-column gap-3'}>
+                <h1>NAV BUTTONS</h1>
+                <div className={'d-flex gap-4'}>
+                    <button onClick={() => navigate('/game/round-1')}>Round 1</button>
+                    <button onClick={() => navigate('/game/round-2')}>Round 2</button>
+                    <button onClick={() => navigate('/game/round-3')}>Round 3</button>
+                    <button onClick={() => navigate('/game/round-4')}>Round 4</button>
+                </div>
+
+                <div className={'d-flex gap-4'}>
+                    <button onClick={() => navigate('/game/sudden-death')}>Sudden Death</button>
+                    <button onClick={() => navigate('/game/fast-money')}>Fast Money</button>
+                </div>
+            </div>
+            <div>
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
         </div>
     );
 }
