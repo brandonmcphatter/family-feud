7b7326c93c0675a828af388421505df6ca262bde
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-10 17:21:48 -0600

    scoreScreen layout set

--- src/pages/ScoreScreen.js
+++ src/pages/ScoreScreen.js
@@ -1,33 +1,60 @@
 import {useNavigate} from "react-router";
 import familyFeudLogo from '../images/big-ff-logo.png'
+import {useGame} from "../context/GameContext";
+import TeamScore from "../components/answerBoard/TeamScore";
 export default function ScoreScreen() {
-
+    const {team1, t1Score, team2, t2Score} = useGame();
     const navigate = useNavigate();
 
     return (
-        <div className='d-flex flex-column  gap-3'>
+        <div className='d-flex flex-column gap-5 animate__animated animate__zoomIn pt-3'>
 
-                <img src={familyFeudLogo} alt={'Family Feud Logo'} width={'70%'}/>
-
-            <div>
+            {/*IMAGE*/}
+            <div className={'d-flex justify-content-center align-items-start'}>
+                <img src={familyFeudLogo} alt={'Family Feud Logo'} width={'50%'} height={'30%'}/>
             </div>
 
-            <div className={'d-flex flex-column gap-3'}>
-                <h1>NAV BUTTONS</h1>
-                <div className={'d-flex gap-4'}>
-                    <button onClick={() => navigate('/game/round-1')}>Round 1</button>
-                    <button onClick={() => navigate('/game/round-2')}>Round 2</button>
-                    <button onClick={() => navigate('/game/round-3')}>Round 3</button>
-                    <button onClick={() => navigate('/game/round-4')}>Round 4</button>
+            <div className={'d-flex justify-content-evenly gap-3'}>
+
+                <div className={'d-flex flex-column gap-4 align-items-center'}>
+                    <h1 className={'text-uppercase text-white fw-bolder'}>{team1}</h1>
+                    <TeamScore teamScore={t1Score}/>
                 </div>
 
-                <div className={'d-flex gap-4'}>
-                    <button onClick={() => navigate('/game/sudden-death')}>Sudden Death</button>
-                    <button onClick={() => navigate('/game/fast-money')}>Fast Money</button>
+                <div className={'d-flex flex-column justify-content-center align-items-center gap-3'}>
+                    <button
+                        className={'btn btn-lg btn-dark px-5 py-3 btn-outline-primary border border-3 rounded-4 border-primary-subtle text-white text-uppercase fw-bolder'}
+                        onClick={() => navigate('/game/round-1')}>Round 1
+                    </button>
+                    <button
+                        className={'btn btn-lg btn-dark px-5 py-3 btn-outline-primary border border-3 rounded-4 border-primary-subtle text-white text-uppercase fw-bolder'}
+                        onClick={() => navigate('/game/round-2')}>Round 2
+                    </button>
+                    <button
+                        className={'btn btn-lg btn-dark px-5 py-3 btn-outline-primary border border-3 rounded-4 border-primary-subtle text-white text-uppercase fw-bolder'}
+                        onClick={() => navigate('/game/round-3')}>Round 3
+                    </button>
+                    <button
+                        className={'btn btn-lg btn-dark px-5 py-3 btn-outline-primary border border-3 rounded-4 border-primary-subtle text-white text-uppercase fw-bolder'}
+                        onClick={() => navigate('/game/round-4')}>Round 4
+                    </button>
+                    <button
+                        className={'btn btn-lg btn-dark px-5 py-3 btn-outline-danger border border-3 rounded-4 border-danger-subtle text-white text-uppercase fw-bolder'}
+                        onClick={() => navigate('/game/sudden-death')}>Sudden Death
+                    </button>
+                    <button
+                        className={'btn btn-lg btn-dark px-5 py-3 btn-outline-success border border-3 rounded-4 border-success-subtle text-white text-uppercase fw-bolder'}
+                        onClick={() => navigate('/game/fast-money')}>Fast Money
+                    </button>
+                </div>
+
+                <div className={'d-flex flex-column gap-4 align-items-center'}>
+                    <h1 className={'text-uppercase text-white fw-bolder'}>{team2}</h1>
+                    <TeamScore teamScore={t2Score} />
                 </div>
             </div>
-            <div>
-                <button onClick={() => navigate('/')}>Home</button>
+            <div className={'d-flex justify-content-end'}>
+            <button className={'btn btn-lg btn-dark px-5 py-3 btn-outline-primary border border-3 rounded-4 border-primary-subtle text-white text-uppercase fw-bolder'} onClick={() => navigate('/')}>Home</button>
             </div>
         </div>
     );
