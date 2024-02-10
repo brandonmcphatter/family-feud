09a0b2e31c793580b8b5742bd1fdd21c1640b4d1
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-10 17:22:28 -0600

    added styling

--- src/components/setup/SetupFastMoneyAnswers.js
+++ src/components/setup/SetupFastMoneyAnswers.js
@@ -3,7 +3,7 @@ import SetupFastMoneyAnswer from "./SetupFastMoneyAnswer";
 export default function SetupFastMoneyAnswers({answers, setAnswers, points, setPoints}) {
 
     return (
-        <div>
+        <div className={'d-flex flex-column gap-4 justify-content-center align-items-center'}>
             {answers.map((a, i) => {
                 return <SetupFastMoneyAnswer answerNum={i} allAnswers={answers} answer={a} points={points[i]} setAnswer={setAnswers} allPoints={points} setPoints={setPoints} key={i}/>
             })}
