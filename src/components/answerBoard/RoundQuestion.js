905bb14976abd25afb7363bb8602fc85f2d39519
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-10 17:21:37 -0600

    proper round question will display

--- src/components/answerBoard/RoundQuestion.js
+++ src/components/answerBoard/RoundQuestion.js
@@ -1,12 +1,12 @@
 import {useGame} from "../../context/GameContext";
 import styles from './RoundQuestion.module.css';
 
-function RoundQuestion() {
+function RoundQuestion({rd}) {
     const {roundQuestionHidden, gameData} = useGame();
 
     return (
         <div className={`${styles.roundQuestion} my-3`}>
-            <h1 className='text-center text-white'>{roundQuestionHidden ? '' : gameData[0].question} </h1>
+            <h1 className='text-center text-white'>{roundQuestionHidden ? '' : gameData[rd].question} </h1>
         </div>
     );
 }
