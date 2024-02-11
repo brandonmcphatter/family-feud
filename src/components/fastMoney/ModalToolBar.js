e4a1e7cfddefdc7eaf8f4adb08eee79c7e0847ca
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-02-11 12:45:23 -0600

    styled buttons

--- src/components/fastMoney/ModalToolBar.js
+++ src/components/fastMoney/ModalToolBar.js
@@ -38,11 +38,11 @@ export default function ModalToolBar({close, save, time}) {
     }
 
     return (
-        <div className={'d-flex justify-content-around'}>
-            <button onClick={toggleTimer}>Clock: {timer}</button>
-            <button onClick={tryAnswerAgain}>&#10060;</button>
-            <button onClick={close} className='fm-button'> close</button>
-            <button onClick={save} className='fm-button'>save</button>
+        <div className={'d-flex justify-content-around mt-4'}>
+            <button className={'btn btn-primary btn-outline-light px-4'} onClick={toggleTimer}>Clock: {timer}</button>
+            <button className={'btn btn-light  btn-outline-dark px-5'} onClick={tryAnswerAgain}>&#10060;</button>
+            <button className={'btn bg-danger btn-outline-dark px-3'} onClick={close} > close</button>
+            <button className={'btn btn-success btn-outline-dark bg-success px-3'} onClick={save}>save</button>
         </div>
     )
 }
